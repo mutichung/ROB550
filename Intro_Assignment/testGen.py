9ce9b1f5d566530cfefd231bdb6373fd475fee17
mutichung <mtchung037@gmail.com>
2019-09-04 15:36:03 -0400

    Finished testGen.py

--- Intro_Assignment/testGen.py
+++ Intro_Assignment/testGen.py
@@ -1,5 +1,22 @@
 import numpy as np
-A = np.array([1, 2, 3])
-print A
-A[1] = 0
-print A
+import sys
+
+def testGen(arg_row, arg_col):
+    try:
+        row = int(arg_row)
+        col = int(arg_col)
+    except ValueError:
+        print 'ERROR: expected two integer arguments.'
+        return
+
+    sol = np.random.standard_normal((row, col))
+    for i in range(row):
+        for j in range(col):
+            print sol[i,j],; sys.stdout.softspace = False
+            if j != col-1:
+                print ', ',; sys.stdout.softspace = False
+        print '\n',; sys.stdout.softspace = False
+    
+
+if __name__ == '__main__':
+    testGen(sys.argv[1], sys.argv[2])
