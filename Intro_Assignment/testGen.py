cab76986d223066519265a71faeb583a4ea1b63f
mutichung <mtchung037@gmail.com>
2019-09-04 15:46:24 -0400

    Comment testGen.py

--- Intro_Assignment/testGen.py
+++ Intro_Assignment/testGen.py
@@ -2,6 +2,13 @@ import numpy as np
 import sys
 
 def testGen(arg_row, arg_col):
+    """
+    Generates a normally-distributed random matrix in CSV format.\n
+    Gets the matrix dimensions, row and column, from the command line.
+    """
+    
+    # Tests whether the input arguments are integers. If yes, turns them from
+    # strings integers; otherwise, raise an ERROR flag and terminates the function.
     try:
         row = int(arg_row)
         col = int(arg_col)
@@ -9,14 +16,19 @@ def testGen(arg_row, arg_col):
         print 'ERROR: expected two integer arguments.'
         return
 
+    # Generates normally-distributed data.
     sol = np.random.standard_normal((row, col))
+
+    # Print the data in CSV format (getting rid of all the line changes and spaces).
     for i in range(row):
         for j in range(col):
             print sol[i,j],; sys.stdout.softspace = False
             if j != col-1:
                 print ', ',; sys.stdout.softspace = False
         print '\n',; sys.stdout.softspace = False
+    return
     
 
 if __name__ == '__main__':
+    # Read command line arguments as function input.
     testGen(sys.argv[1], sys.argv[2])
